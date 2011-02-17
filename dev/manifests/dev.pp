22aa81b972ce18ee10479bf4ce9b37113ba901c5
Howard Tyson <howard@howardtyson.com>
2011-02-17 12:16:59 -0500

    reorganized, added solr module draft

--- dev/manifests/dev.pp
+++ dev/manifests/dev.pp
@@ -2,8 +2,14 @@ class dev {
   package { 
     [
       "build-essential",
+      "ntp",
     ]: 
       ensure => installed 
+  }
+  cron { logrotate:
+    command => "ntpdate ntp.ubuntu.com",
+      user => root,
+      minute => '*/10'
   }
 }
 
