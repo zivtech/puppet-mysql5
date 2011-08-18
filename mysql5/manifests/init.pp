185d731536c15b69ab17e528eee05bfbe9a2ed93
Howard Tyson <howard@zivtech.com>
2011-08-18 00:12:07 -0400

    updated samba config and renamed all module manifests to init.pp to fix a loading error with recent versions of puppet

--- mysql5/manifests/init.pp
+++ mysql5/manifests/init.pp
@@ -1,1 +1,37 @@
-import "*"
+class mysql5 {
+  package { 
+    [
+      "mysql-server",
+      "mysql-client",
+      "mysql-common",
+    ]: 
+      ensure => installed 
+  }
+  service {"mysql":
+    ensure => running,
+  }
+  exec { "Set MySQL server root password":
+    subscribe => [ 
+      Package["mysql-server"],
+    ],
+    refreshonly => true,
+    unless => "mysqladmin -uroot -p$mysqlpassword status",
+    path => "/bin:/usr/bin",
+    command => "mysqladmin -uroot password $mysqlpassword",
+  }
+
+  file { "webadmin-mycnf":
+    path => "/home/webadmin/.my.cnf",
+    content => template("mysql5/my.cnf.erb"),
+    owner => webadmin,
+  }
+
+  file { "root-mycnf":
+    path => "/root/.my.cnf",
+    content => template("mysql5/my.cnf.erb"),
+    owner => webadmin,
+  }
+}
+
+
+
