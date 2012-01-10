06cca90d56bf977ecf80c8b3c70f20d1fbe93310
Howard Tyson <howard@howardtyson.com>
2012-01-10 13:53:24 -0500

    Fixing class extension error.

--- mysql5-dev/manifests/init.pp
+++ mysql5-dev/manifests/init.pp
@@ -1,4 +1,4 @@
-class mysql5-dev ($mysqlpassword) inherits mysql5 {
+class mysql5-dev ($mysqlpassword) inherits mysql5 ($mysqlpassword) {
 
   File['my.cnf'] {
     source => "puppet:///modules/mysql5-dev/my.cnf",
