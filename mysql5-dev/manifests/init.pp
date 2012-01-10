4cf15f42bfcf1c393421632839ae12b42978eae6
Howard Tyson <howard@howardtyson.com>
2012-01-10 15:30:19 -0500

    Tweaked mysql dev module.

--- mysql5-dev/manifests/init.pp
+++ mysql5-dev/manifests/init.pp
@@ -1,4 +1,4 @@
-class mysql5-dev ($mysqlpassword) inherits mysql5 ($mysqlpassword) {
+class mysql5-dev ($mysqlpassword) inherits mysql5 {
 
   File['my.cnf'] {
     source => "puppet:///modules/mysql5-dev/my.cnf",
