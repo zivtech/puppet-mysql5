96e87d48829cd55f2238d6c79a3a7acdbf3eadb0
Howard Tyson <howard@howardtyson.com>
2012-05-16 12:50:58 -0400

    Updating my.cnf source for dev module.

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,7 +1,7 @@
 class mysql5-dev ($mysqlpassword) inherits mysql5 {
 
   File['my.cnf'] {
-    source => "puppet:///modules/mysql5-dev/my.cnf",
+    source => "puppet:///modules/mysql5/dev.my.cnf",
   }
 
 }
