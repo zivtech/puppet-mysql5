7f1460dd9e570c26b153fabe6b19fcb23d604ea2
acouch <aaron@zivtech.com>
2012-06-05 12:11:25 -0500

    Removing parametized password for dev.pp and removing call to mysql5 class

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,7 +1,5 @@
-class mysql5::dev($mysqlpassword) inherits mysql5 {
-  class { 'mysql5':
-    mysqlpassword => $mysqlpassword,
-  }
+# Development MySQL settings
+class mysql5::dev inherits mysql5 {
   File['my.cnf'] {
     source => "puppet:///modules/mysql5/dev.my.cnf",
   }
