c758b569da66b23adf1467cd42995068aa785401
Howard Tyson <howard@howardtyson.com>
2012-07-09 09:00:44 -0400

    Update to make parameter inheritance slightly less crazy...

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,5 +1,5 @@
 # Development MySQL settings
-class mysql5::dev inherits mysql5 {
+class mysql5::dev ($mysqlpassword = $mysql5::password, $webadminuser = $mysql5::webadminuser, $webadmingroup = $mysql5::webadmingroup) inherits mysql5 {
   File['my.cnf'] {
     source => "puppet:///modules/mysql5/dev.my.cnf",
     notify => Service["mysql"],
